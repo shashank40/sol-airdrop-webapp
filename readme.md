d7923e9e320b85adc2b1be4d48e49bb39e6cdcf1
shashank tiwari <shashanktiwari40@gmail.com>
2023-03-31 23:41:51 +0530

    Code for airdrop webapp

---
+++ readme.md
@@ -0,0 +1,5 @@
+Use this link to access the airdrop website : 
+
+Through this you can actually airdrop yourself Testnet/Devnet solana.
+
+To run the project, simply clone it and run the index.html on your local.
---
+++ webapp/main.js
@@ -0,0 +1,48 @@
+document.getElementById("submitButton1").addEventListener("click", submit);
+
+function submit(){
+    let address = document.querySelector("#address")
+    let count = document.querySelector("#count")
+    let message = document.querySelector("#result");
+    let network = document.querySelector('#networkChoice')
+
+    console.log(network.value);
+
+    if(address.value==null || count.value==null || count.value==undefined || address.value==undefined){
+    message.innerHTML = 'Enter both address and amount'; 
+    return
+    }
+    try {
+        let publicKey = new PublicKey(address.value);
+        console.log(publicKey)
+        let connection = new Connection(network.value, "confirmed");
+
+        if(!connection || connection==null){
+        message.innerHTML = `No Account found with this ${address}` 
+        return 
+        }
+
+        let amount = Number(count.value)
+
+        let signature = connection.requestAirdrop(publicKey, amount*LAMPORTS_PER_SOL);
+        let latestBlockHash =  connection.getLatestBlockhash();
+
+        connection.confirmTransaction({blockhash:latestBlockHash.blockhash, 
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: signature});
+        
+        let response = Promise.resolve(connection.getAccountInfo(publicKey))
+        response.then(
+            function(value) {
+                message.innerHTML = `You have ${network.options[network.selectedIndex].text} solana count = ${value.lamports/LAMPORTS_PER_SOL}`;
+            },
+            function(error){
+                message.innerHTML = error.message
+            }
+        );
+    }
+    catch(err){
+        message.innerHTML = err.message
+    }
+
+}
